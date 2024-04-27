86b043a8ba2e8b5176bf0cb12f518951e6c26445
Anas Mazouni <madadj4@gmail.com>
2024-04-27 22:33:22 +0200

    ci: automatically build and release

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,16 +2,10 @@ import { cleanEnv, str } from 'envalid';
 
 export const env = cleanEnv(process.env, {
   BOT_TOKEN: str(),
-  APPLICATION_ID: str({
-    default: '1211793089321242684',
-  }),
+  APPLICATION_ID: str(),
   DB_NAME: str({
     default: 'db.sqlite',
   }),
-  ADMINS: str({
-    default: '447912513653309442',
-  }),
-  WHITELISTED_ROLE_ID: str({
-    default: '1184529777563017469',
-  }),
+  ADMINS: str(),
+  WHITELISTED_ROLE_ID: str(),
 });
